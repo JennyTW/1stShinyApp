4f16e2a95b038da8219f8d6f3595ccb09693bfad
JennyTW <foorong@fus-macbook-air.gateway.pace.com>
2016-05-08 21:30:59 -0500

    Update Shiny App

--- ui.R
+++ ui.R
@@ -11,6 +11,7 @@ shinyUI(
                     ),
 
       mainPanel(  plotOutput("distPlot1") )
-          )
+          ),
+    h3 ("From Central Limit Theorem, as the sample size (n) increase, the distribution of sample mean will approach normal distribution.In this Shiny App, we can observe the difference of exponential distribution with difference sample size by specifing N1 and N2. We generate random sample variables with one thousand simulations and specify lambda=0.2. ")
   ))
 
